8684ebafec5be68470eedce53b02c69b498e39f7
mubarak23 <mubarakaminu340@gmail.com>
2019-11-13 10:43:53 +0100

    updae, delete, get article

--- src/resource/article/article.controller.js
+++ src/resource/article/article.controller.js
@@ -1,11 +1,11 @@
-const article = require('article');
+const Article = require('./article');
 
-exports.createArticle = (req, res, next) => {
-    const Thing = new thing({
+exports.createArticle = (req, res) => {
+    const Article = new Article({
         title: req.body.title,
         body: req.body.body
     });
-    article.save().then(
+    Article.save().then(
         () =>{
             res.status(201).json({
                 status: "success",
@@ -25,3 +25,67 @@ exports.createArticle = (req, res, next) => {
     });
 } 
 
+exports.upadteArticle = (req, res) =>{
+    const id = req.params.id;
+    const Article_modefied = {
+        title: req.body.title,
+        body: req.body.body
+    };
+    Article.updateOne({_id: id, Article_modefied}).then(
+        () =>{
+            res.status(201).json({
+                status: "success",
+                data: {
+                    message: "Article Upated Successfully",
+                    title: Article.title,
+                    body: article.body
+                }
+            })
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                status: error,
+                error: "Unable to updated article"
+            });
+        }
+    );
+}
+
+exports.deleteArticle = (req, res) =>{
+    Article.findOneAndDelete({_id: req.params.id}).then(
+        () =>{
+            res.status(200).json({
+                status: 'Success',
+                data: {
+                    message: 'Article Deleted Successfully',
+                }
+            });
+        }
+    ).catch(
+        (error) =>{
+            res.status(400).json({
+                status: error,
+                error: "Unable to delete article"
+            });
+        }
+    )
+}
+
+exports.getArticles = (req, res) =>{
+        Article.find().then(
+            (Article) =>{
+                res.status(200).json({
+                  status: "success",
+                  data: Article  
+                })
+            }
+        ).catch(
+            (error) => {
+                res.status(400).json({
+                    status: error,
+                    error: "Unable to fetch lists of articles"
+                });
+            }
+        )
+}
